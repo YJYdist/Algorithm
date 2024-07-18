726f245218fc4ce2c62ebfaaa47217756d95e73a
YJYdist <yjydist@gmail.com>
2024-07-18 16:22:46 +0800

    Commmit on 2024-7-18

--- LuoGu/p1217.cpp
+++ LuoGu/p1217.cpp
@@ -1,3 +1,5 @@
+/*
+以下做法会超时
 #include <iostream>
 #include <string>
 #include <vector>
@@ -58,6 +60,77 @@ int main()
     }
 
     // 输出
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << endl;
+    }
+
+    return 0;
+}
+*/
+
+/*依然有超时的情况*/
+#include <vector>
+#include <iostream>
+using namespace std;
+// 判断位数(除了11之外,所有回文素数的位数都不是偶数)
+bool judge_weishu(int k)
+{
+    if (k >= 10 && k < 100 && k != 11 || k >= 1000 && k < 10000)
+        return false;
+    if (k >= 100000 && k < 1000000 || k >= 10000000 && k < 100000000)
+        return false;
+    return true;
+}
+
+// 判断回文
+bool judge_huiwen(int k)
+{
+
+    int a[10], i = 0;
+    while (k > 0)
+    {
+        a[i] = k % 10;
+        k /= 10;
+        i++;
+    }
+    for (int j = 0; j < i; j++)
+        if (a[j] != a[i - j - 1])
+            return false;
+    return true;
+}
+// 判断素数
+bool judge_sushu(int num)
+{
+    if (num == 2)
+    {
+        return true;
+    }
+
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    vector<int> res;
+
+    for (int i = a; i <= b; i++)
+    {
+        if (judge_weishu(i) && judge_huiwen(i) && judge_sushu(i))
+        {
+            res.push_back(i);
+        }
+    }
+
     for (int i = 0; i < res.size(); i++)
     {
         cout << res[i] << endl;
